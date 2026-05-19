e072843b274f820330d3f9ac7aad7ae83d6afa86
Lucas <lucasfulks@gmail.com>
2026-05-19 10:30:57 -0500

    chore: add QueryClientProvider wrapping KeyboardShortcutsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { KeyboardShortcutsProvider } from '@/components/KeyboardShortcutsProvider';
 import App from './App';
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 30_000, retry: 1, refetchOnWindowFocus: false },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <KeyboardShortcutsProvider>
-          <App />
-        </KeyboardShortcutsProvider>
+        <QueryClientProvider client={queryClient}>
+          <KeyboardShortcutsProvider>
+            <App />
+          </KeyboardShortcutsProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
